b966a55579ae18fc7818b5987448e92811a86a79
maarink <npmaari25@gmail.com>
2022-04-28 22:40:42 +0530

    project structure

---
+++ src/org/company/CompanyInfo.java
@@ -0,0 +1,19 @@
+package org.company;
+
+public class CompanyInfo {
+private void companyName() {
+	System.out.println("google");
+}
+private void companyId() {
+	System.out.println("256");
+}
+private void companyAddress() {
+	System.out.println("USA");
+}
+public static void main(String[] args) {
+	CompanyInfo a=new CompanyInfo();
+	a.companyName();
+	a.companyId();
+	a.companyAddress();
+}
+}
